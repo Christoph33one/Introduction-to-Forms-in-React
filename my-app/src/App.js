a6491318c643d7dd1efe98e01b082eafecf6239d
Christoph33one <cjbrowning23@hotmail.com>
2023-03-23 18:30:53 +0000

    added search bar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 // import ControlledForm from './components/ControlledForm';
 import UncontrolledForm from './components/UncontrolledForm';
+import SearchBar1 from './components/SearchBar1';
 
 
 function App() {
   return (
     <div>
       {/* <ControlledForm /> */}
-      <UncontrolledForm />
+      {/* <UncontrolledForm /> */}
+      < SearchBar1 />
     </div>
   );
 }
